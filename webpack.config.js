974095a061ceb50d660c2ff7cfcefc9bd7e9dec3
nickylau82 <70458055@qq.com>
2018-02-25 20:43:45 +0800

    add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
-module.exports = {
+const path = require('path');
+// https://doc.webpack-china.org/guides/output-management/#-dist-
+// 清理文件夹的插件
+const cleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = {
 	entry: {
 		app: './src/app.js'
 	}, 
 
 	output: {
-		filename: '[name].[hash:8].js'
-	}
+		filename: '[name].[hash:8].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	plugins: [
+		// 参数是个数组，传入目录名称
+		new cleanWebpackPlugin(['dist'])
+	]
 }
+
+module.exports = config;
